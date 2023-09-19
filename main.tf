8885108edfc74d49f5ac3a1f78a1ab33ca7c7d5e
Andy Assareh <assareh@hashicorp.com>
2023-09-19 10:20:25 -0700

    moving to cloud init

--- main.tf
+++ main.tf
@@ -130,62 +130,17 @@ resource "aws_instance" "hashicat" {
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
+  user_data = templatefile(
+    "${path.module}/templates/deploy_app.tpl",
+    {
+      tpl_prefix      = var.prefix
+      tpl_width       = var.width
+      tpl_height      = var.height
+      tpl_placeholder = var.placeholder
+  })
+
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-  }
-}
-
-# We're using a little trick here so we can run the provisioner without
-# destroying the VM. Do not do this in production.
-
-# If you need ongoing management (Day N) of your virtual machines a tool such
-# as Chef or Puppet is a better choice. These tools track the state of
-# individual files and can keep them in the correct configuration.
-
-# Here we do the following steps:
-# Sync everything in files/ to the remote VM.
-# Set up some environment variables for our script.
-# Add execute permissions to our scripts.
-# Run the deploy_app.sh script.
-resource "null_resource" "configure-cat-app" {
-  depends_on = [aws_eip_association.hashicat]
-
-  triggers = {
-    build_number = timestamp()
-  }
-
-  provisioner "file" {
-    source      = "files/"
-    destination = "/home/ubuntu/"
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = tls_private_key.hashicat.private_key_pem
-      host        = aws_eip.hashicat.public_ip
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt -y update",
-      "sleep 15",
-      "sudo apt -y update",
-      "sudo apt -y install apache2",
-      "sudo systemctl start apache2",
-      "sudo chown -R ubuntu:ubuntu /var/www/html",
-      "chmod +x *.sh",
-      "PLACEHOLDER=${var.placeholder} WIDTH=${var.width} HEIGHT=${var.height} PREFIX=${var.prefix} ./deploy_app.sh",
-      "sudo apt -y install cowsay",
-      "cowsay Mooooooooooo!",
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = tls_private_key.hashicat.private_key_pem
-      host        = aws_eip.hashicat.public_ip
-    }
   }
 }
 
--- templates/deploy_app.tpl
+++ templates/deploy_app.tpl
@@ -2,6 +2,12 @@
 # Script to deploy a very simple web application.
 # The web app has a customizable image and some text.
 
+sudo apt -y update
+sleep 15
+sudo apt -y update
+sudo apt -y install apache2
+sudo systemctl start apache2
+sudo chown -R ubuntu:ubuntu /var/www/html
 cat << EOM > /var/www/html/index.html
 <html>
   <head><title>Meow!</title></head>
@@ -9,9 +15,9 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="http://${tpl_placeholder}/${tpl_width}/${tpl_height}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${tpl_prefix}'s app. Replace this text with your own.
   <!-- END -->
 
   </div>
